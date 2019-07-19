5cb36c03928c0768ba13054a6298b5380bbc7884
Carl Barringer <cbarringer@Carls-MacBook-Pro.local>
2019-07-19 16:00:55 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,31 @@
+const trucks = [
+    {
+        date: "07/20/2019",
+        manufacturer: "Ford",
+        model: "F150",
+        color: "red",
+    },
+    {
+        date: "07/20/2019",
+        manufacturer: "Ford",
+        model: "F250",
+        color: "blue",
+    },
+    {
+        date: "07/20/2019",
+        manufacturer: "Dodge",
+        model: "2500",
+        color: "black",
+    },
+    {
+        date: "07/20/2019",
+        manufacturer: "Chevrolet",
+        model: "1500",
+        color: "white",
+    },
+
+
+
+
+
+]
